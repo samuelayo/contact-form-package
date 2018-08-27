97ff324f3a08837029ae48e5cea116b4b4939cb6
samuel ogundipe <ayoogundipe2005@gmail.com>
2018-08-27 17:18:55 +0100

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 
 <?php
-Route::group(['namespace' => 'Samuelayo\Contactform\Http\Controllers'], function(){
+Route::group(['namespace' => 'Samuelayo\Contactform\Http\Controllers', 'middleware' => ['web']], function(){
     Route::get('contact', 'ContactFormController@index');
     Route::post('contact', 'ContactFormController@sendMail')->name('contact');
 });
